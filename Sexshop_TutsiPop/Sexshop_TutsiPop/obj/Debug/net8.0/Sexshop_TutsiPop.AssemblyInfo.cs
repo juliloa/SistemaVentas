31cdf272ddd6154ac42408a1dfef850c4f100728
juliloa <juliana.loaiza14@gmail.com>
2024-10-27 12:22:35 -0500

    nuevos cambios

--- Sexshop_TutsiPop/Sexshop_TutsiPop/obj/Debug/net8.0/Sexshop_TutsiPop.AssemblyInfo.cs
+++ Sexshop_TutsiPop/Sexshop_TutsiPop/obj/Debug/net8.0/Sexshop_TutsiPop.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Sexshop_TutsiPop")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b9b25bf780db08966f33b37f25fe91d63e3fb27d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4f75a881ee0b10c6ccec1dd6dfa7cc7642191a93")]
 [assembly: System.Reflection.AssemblyProductAttribute("Sexshop_TutsiPop")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Sexshop_TutsiPop")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

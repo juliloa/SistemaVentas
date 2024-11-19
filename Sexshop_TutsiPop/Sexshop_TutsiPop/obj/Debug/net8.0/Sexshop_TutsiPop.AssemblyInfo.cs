24081a284c74bc2617ea1627a12a95c437fab967
juliloa <juliana.loaiza14@gmail.com>
2024-11-18 21:48:30 -0500

    base URL

--- Sexshop_TutsiPop/Sexshop_TutsiPop/obj/Debug/net8.0/Sexshop_TutsiPop.AssemblyInfo.cs
+++ Sexshop_TutsiPop/Sexshop_TutsiPop/obj/Debug/net8.0/Sexshop_TutsiPop.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Sexshop_TutsiPop")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ebdb196dffd78b013c04b28844e064a59c5992db")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b8c1c9aecfbde8d844f78e0886a236baba8f0d1e")]
 [assembly: System.Reflection.AssemblyProductAttribute("Sexshop_TutsiPop")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Sexshop_TutsiPop")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
